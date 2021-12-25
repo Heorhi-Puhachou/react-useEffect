7bbaab18bb434a1372e96c1c8989698018f02fd7
Heorhi Puhachou <h.puhachou@gmail.com>
2021-12-25 11:40:14 +0300

    Cleanup example.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
     const [formIsValid, setFormIsValid] = useState(false);
 
     useEffect(() => {
-        setFormIsValid(
-            enteredEmail.includes('@') && enteredPassword.trim().length > 6
-        );
+        const identifier = setTimeout(() => {
+            console.log('CHEKING!!!');
+            setFormIsValid(
+                enteredEmail.includes('@') && enteredPassword.trim().length > 6
+            );
+        }, 500);
+
+        return () => {
+            clearTimeout(identifier);
+            console.log("CleaNUp");
+        }
 
     }, [enteredEmail, enteredPassword]);
 
